88af036081414e627ea2ff5022f05594b49eff17
woongpang <jr.woong@gmail.com>
2023-12-16 15:42:42 +0900

    add JWT refresh

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users.views import SignUpView, CustomTokenObtainPairView, GoogleLogin, AuthCheck
-
+from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup_view"),
     path(
@@ -10,5 +10,5 @@ urlpatterns = [
     ),
     path("auth/", AuthCheck.as_view(), name="auth_check"),
     path("google/", GoogleLogin.as_view(), name="google_login"),
-    
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
